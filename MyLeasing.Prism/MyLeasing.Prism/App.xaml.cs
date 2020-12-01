525fd12f4ce8a7ef9e04cd350f44db3de7ab5fd6
Emerson palacio <emersonpalaciootalvaro@gmail.com>
2020-12-01 12:55:48 -0500

    initialproyect

---
+++ MyLeasing.Prism/MyLeasing.Prism/App.xaml.cs
@@ -0,0 +1,33 @@
+using MyLeasing.Prism.ViewModels;
+using MyLeasing.Prism.Views;
+using Prism;
+using Prism.Ioc;
+using Xamarin.Essentials.Implementation;
+using Xamarin.Essentials.Interfaces;
+using Xamarin.Forms;
+
+namespace MyLeasing.Prism
+{
+    public partial class App
+    {
+        public App(IPlatformInitializer initializer)
+            : base(initializer)
+        {
+        }
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+
+            await NavigationService.NavigateAsync("NavigationPage/MainPage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterSingleton<IAppInfo, AppInfoImplementation>();
+
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+        }
+    }
+}
