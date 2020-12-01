154efc34230a07cbcb0fb4e370c2a6f24875492a
Emerson palacio <emersonpalaciootalvaro@gmail.com>
2020-12-01 15:40:22 -0500

    initialCarreteraDestapada

---
+++ MyLeasing.Common/Entitites/Country.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyLeasing.Common.Entitites
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        [Required]
+        public string Name { get; set; }
+    }
+}
---
+++ MyLeasing.Web/Data/DataContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyLeasing.Common.Entitites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyLeasing.Web.Data
+{
+    public class DataContext :DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Country>()
+                .HasIndex(t => t.Name)
+                .IsUnique();
+        }
+
+    }
+}
