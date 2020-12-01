68ed99e44da0a92597e90f41b4bf955c25a50305
Emerson palacio <emersonpalaciootalvaro@gmail.com>
2020-12-01 16:07:23 -0500

    iniciaCarreteradetapada

---
+++ MyLeasing.Web/Data/SeedDb.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyLeasing.Web.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _dataContext;
+
+        public SeedDb(DataContext  dataContext)
+        {
+            this._dataContext = dataContext;
+        }
+
+        public async Task SeedAsync()
+        {
+            await _dataContext.Database.EnsureCreatedAsync();
+        }
+
+    }
+}
