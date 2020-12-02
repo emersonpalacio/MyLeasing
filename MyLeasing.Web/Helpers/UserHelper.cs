b22696b79aad80ac117caaa35c7821a4d706caba
Emerson palacio <emersonpalaciootalvaro@gmail.com>
2020-12-02 17:40:06 -0500

    finallyLisPubli

--- MyLeasing.Web/Helpers/UserHelper.cs
+++ MyLeasing.Web/Helpers/UserHelper.cs
@@ -138,5 +138,16 @@ namespace MyLeasing.Web.Helpers
         }
 
 
+        public async Task<string> GeneratePasswordResetTokenAsync(User user)
+        {
+            return await _userManager.GeneratePasswordResetTokenAsync(user);
+        }
+
+        public async Task<IdentityResult> ResetPasswordAsync(User user, string token, string password)
+        {
+            return await _userManager.ResetPasswordAsync(user, token, password);
+        }
+
+
     }
 }
