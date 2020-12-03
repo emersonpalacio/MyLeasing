3a79d4c27a82302b1801a49b6d86e230c5151cf6
Emerson palacio <emersonpalaciootalvaro@gmail.com>
2020-12-03 09:22:34 -0500

    getOwnerApi

---
+++ MyLeasing.Common/Models/PropertyImageResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLeasing.Common.Models
+{
+    public class PropertyImageResponse
+    {
+        public int Id { get; set; }
+        public string ImageUrl { get; set; }
+
+    }
+}
---
+++ MyLeasing.Web/Controllers/API/PropertyTypesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyLeasing.Web.Data;
+using MyLeasing.Web.Data.Entities;
+
+namespace MyLeasing.Web.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PropertyTypesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public PropertyTypesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/PropertyTypes
+        [HttpGet]
+        public IEnumerable<PropertyType> GetPropertyTypes()
+        {
+            return _context.PropertyTypes.OrderBy(p=> p.Name);
+        }
+
+        // GET: api/PropertyTypes/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPropertyType([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var propertyType = await _context.PropertyTypes.FindAsync(id);
+
+            if (propertyType == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(propertyType);
+        }
+
+    }
+}
